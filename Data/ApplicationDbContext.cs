a2165132edfc97f807cc27714b3635476d28b4fd
Tomas1-2 <tsabena84@gmail.com>
2023-05-15 16:44:45 -0300

    first migration

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using El_Rincon_De_La_Copa.Models;
+namespace El_Rincon_De_La_Copa.Data;
+public class ApplicationDbContext : IdentityDbContext
+{
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace El_Rincon_De_La_Copa.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
