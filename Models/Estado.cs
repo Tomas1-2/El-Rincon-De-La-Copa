3cce7308e9aaf1651338d3fb1792fcf019e8f021
Tomas1-2 <tsabena84@gmail.com>
2023-05-16 21:03:01 -0300

    ModelosNuevos✌✌✌

---
+++ Models/Estado.cs
@@ -0,0 +1,8 @@
+namespace El_Rincon_De_La_Copa.Models;
+
+public class Estado
+{
+    public bool Entregado { get; set; }
+    public bool EnCurso { get; set; }
+    public bool NoEnviado { get; set; }
+}
